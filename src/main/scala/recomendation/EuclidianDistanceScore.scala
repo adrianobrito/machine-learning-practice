3fc1c81769ab4427cfff58e0275f041eeb2eb2a8
Adriano Brito <adrianosdebrito@gmail.com>
2016-12-22 13:49:51 -0300

    Implementing Euclidian Distance Score

--- src/main/scala/recomendation/EuclidianDistanceScore.scala
+++ src/main/scala/recomendation/EuclidianDistanceScore.scala
@@ -5,8 +5,26 @@ package recomendation
   */
 object EuclidianDistanceScore {
 
-  def similarityDistance() = {
+  def similarityDistance[T](movies:List[Map[String, Any]], person1:Int, person2:Int) : Double = {
+    val filterByUserId     = (userId:Int) => {
+      (movie:Map[String, Any]) => {
+        val userId = movie("userId").asInstanceOf[Int]
+        userId == userId
+      }
+    }
 
+    val person1Movies = movies.filter(filterByUserId(person1))
+    val person2Movies = movies.filter(filterByUserId(person2))
+
+    val similarMovies:List[Map[String, Any]] = person1Movies
+      .filter( person1Movie => person2Movies.exists(person2Movie => person1Movie("userId") == person2Movie("userId")))
+      .map( similarMovie => Map("movieId" -> similarMovie("movieId"), "similarity" -> 1))
+
+    if(similarMovies.length == 0){
+      return 0.0
+    }
+
+    0.1
   }
 
 }
---
+++ src/main/scala/recomendation/RecomendationAlgorithm.scala
@@ -0,0 +1,10 @@
+package recomendation
+
+/**
+  * Created by adriano.brito on 22/12/2016.
+  */
+trait RecomendationAlgorithm {
+
+
+
+}
